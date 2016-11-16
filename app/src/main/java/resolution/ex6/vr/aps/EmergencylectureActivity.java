52584e86801779cdbee7877af8e0e35b028a9925
inevitable23 <inevitable0223@gmail.com>
2016-11-16 14:09:29 +0900

    kdw_2016_11_16 09:55 폰트 커스텀 하는중

--- app/src/main/java/resolution/ex6/vr/aps/EmergencylectureActivity.java
+++ app/src/main/java/resolution/ex6/vr/aps/EmergencylectureActivity.java
@@ -18,14 +18,16 @@ public class EmergencylectureActivity extends AppCompatActivity {
     Button emergency_button2;
     Button emergency_button3;
     Button emergency_button4;
+    Button prevButton;
+    Button nextButton;
+    int currentPage = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergencylecture);
-        emergency_button1 = (Button)findViewById(R.id.emergency_button1);
-        emergency_button2 = (Button)findViewById(R.id.emergency_button2);
-        emergency_button3 = (Button)findViewById(R.id.emergency_button3);
-        emergency_button4 = (Button)findViewById(R.id.emergency_button4);
+        layoutinit();
+
         if (findViewById(R.id.container) != null) {
             if (savedInstanceState != null) {
                 return;
@@ -45,8 +47,9 @@ public class EmergencylectureActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 getSupportFragmentManager().beginTransaction().
-                        replace(R.id.container,emergency1Fragment)
+                        replace(R.id.container, emergency1Fragment)
                         .commit();
+                currentPage = 1;
             }
         });
 
@@ -54,27 +57,75 @@ public class EmergencylectureActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 getSupportFragmentManager().beginTransaction().
-                        replace(R.id.container,emergency2Fragment)
+                        replace(R.id.container, emergency2Fragment)
                         .commit();
+                currentPage = 2;
             }
         });
         emergency_button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 getSupportFragmentManager().beginTransaction().
-                        replace(R.id.container,emergency3Fragment)
+                        replace(R.id.container, emergency3Fragment)
                         .commit();
+                currentPage = 3;
             }
         });
         emergency_button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 getSupportFragmentManager().beginTransaction().
-                        replace(R.id.container,emergency4Fragment)
+                        replace(R.id.container, emergency4Fragment)
                         .commit();
+                currentPage = 4;
+            }
+        });
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int fragmentId = (currentPage + 1);
+                if(fragmentId == 5) fragmentId = 1;
+                Fragment fragment = findFragmentId(fragmentId);
+                getSupportFragmentManager().beginTransaction().
+                        replace(R.id.container, fragment)
+                        .commit();
+                currentPage = fragmentId;
+            }
+        });
+        prevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int fragmentId = currentPage - 1;
+                if (fragmentId == 0) fragmentId = 4;
+                Fragment fragment = findFragmentId(fragmentId);
+                getSupportFragmentManager().beginTransaction().
+                        replace(R.id.container, fragment)
+                        .commit();
+                currentPage = fragmentId;
             }
         });
 
     }
 
+    public void layoutinit() {
+        emergency_button1 = (Button) findViewById(R.id.emergency_button1);
+        emergency_button2 = (Button) findViewById(R.id.emergency_button2);
+        emergency_button3 = (Button) findViewById(R.id.emergency_button3);
+        emergency_button4 = (Button) findViewById(R.id.emergency_button4);
+        prevButton = (Button) findViewById(R.id.prevButton);
+        nextButton = (Button) findViewById(R.id.nextButton);
+    }
+
+    public Fragment findFragmentId(int fragmentId) {
+        if (fragmentId == 1) {
+            return emergency1Fragment;
+        } else if (fragmentId == 2) {
+            return emergency2Fragment;
+        } else if (fragmentId == 3) {
+            return emergency3Fragment;
+        } else if (fragmentId == 4) {
+            return emergency4Fragment;
+        }
+        return new Fragment();
+    }
 }
